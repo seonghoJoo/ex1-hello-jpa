31a95baf05201ea781b5e7127aa1faad1b60cc58
주성호 <etkl8888@naver.com>
2022-01-03 01:20:29 +0900

    UPDATE 영속성 전이(CASCADE) @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL)

---
+++ ex1hellojpa/src/main/java/com/vo/Child.java
@@ -0,0 +1,23 @@
+package com.vo;
+
+import javax.persistence.*;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Child {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name="PARENT_ID")
+    private Parent parent;
+
+}
