fb6d0537c247c50aa723af3f57bb2748c832d756
etkl8888 <tjdgh23712021B@@>
2022-01-05 19:18:38 +0900

    UPDATE Address 기본 생성자 추가

--- ex1hellojpa/src/main/java/com/vo/Address.java
+++ ex1hellojpa/src/main/java/com/vo/Address.java
@@ -1,16 +1,30 @@
 package com.vo;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.Embeddable;
 
 @Getter
-@Setter
 @Embeddable
+@Setter
+// 불변객체로 남기기 위하여 Setter 제거
+// 불변이라는 작은 제약으로 부작용이라는 큰 재앙 막기
 public class Address {
 
     private String city;
     private String street;
     private String zipcode;
+
+    // INFO: HHH000182: No default (no-argument) constructor for class: com.vo.Address (class must be instantiated by Interceptor)
+    // 오류 해결
+    public Address() {
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
